aba37dc5f088a212443a9c23d1523c7a0c516d39
陆玄青 <lu.xuanqing@outlook.com>
2018-05-16 21:39:52 +0800

    beautiful

--- server/run.py
+++ server/run.py
@@ -4,20 +4,26 @@ from btsow import get_links
 
 app = Flask(__name__)
 
+
 @app.route('/info/<bangou>')
 def info(bangou):
+    
     pics = get_pics(bangou)
     links = get_links(bangou)
     info = {'pics': pics, 'links': links}
-    # 允许跨域
     res = jsonify(info)
+    # 允许跨域
     res.headers['Access-Control-Allow-Origin'] = '*'
     return res
+
 
 @app.route('/content')
 def content():
     res = make_response(send_file('content.html'))
+    # 允许跨域
     res.headers['Access-Control-Allow-Origin'] = '*'
     return res
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
