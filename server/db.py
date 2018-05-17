46a4428159aedf8c5af986df5778ba385598b6e9
陆玄青 <lu.xuanqing@outlook.com>
2018-05-17 16:09:50 +0800

    fix a bug

--- server/db.py
+++ server/db.py
@@ -7,19 +7,20 @@ dbpath = '/Users/luxuanqing/FE/javlib/server/jav.db'
 def is_exist(id):
     with sqlite3.connect(dbpath) as conn:
         c = conn.cursor()
-        c.execute('SELECT * FROM movies WHERE id=?', (id,))
+        c.execute('SELECT * FROM movies WHERE id=?', (id, ))
         if c.fetchone():
             return True
         else:
             return False
 
+
 def get_preview_by_id(id):
     with sqlite3.connect(dbpath) as conn:
         c = conn.cursor()
-        c.execute('SELECT preview FROM movies WHERE id=?', (id, ))
-        value = c.fetchone()
-        if value:
-            return json.loads(value[0])
+        c.execute('SELECT id,preview FROM movies WHERE id=?', (id, ))
+        _, preview = c.fetchone()
+        if preview:
+            return json.loads(preview)
         else:
             return None
 
@@ -36,5 +37,4 @@ def update_preview(id, preview):
     with sqlite3.connect(dbpath) as conn:
         c = conn.cursor()
         preview_str = json.dumps(preview)
-        c.execute('UPDATE movies SET preview=? WHERE id=?',
-                  (preview_str, id))
+        c.execute('UPDATE movies SET preview=? WHERE id=?', (preview_str, id))
