fa14b1d830a2edf25e44ab1be6768b1a259ce3af
陆玄青 <lu.xuanqing@outlook.com>
2019-04-14 02:51:46 +0800

    添加代理并优化

--- javbus.py
+++ javbus.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+BASE_URL = 'https://www.javbus.com/'
 HEADERS = {
     'accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -14,38 +14,45 @@ HEADERS = {
     'user-agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'
 }
+PROXIES = {
+    'http': 'http://127.0.0.1:1087',
+    'https': 'http://127.0.0.1:1087',
+}
 
 
 def get_html(url):
     try:
-        r = requests.get(url)
+        r = requests.get(url, timeout=5, proxies=PROXIES)
         return r.text
     except Exception as err:
         print(err)
         return ''
 
 
-def get_url(id):
-    base_url = 'https://www.javbus.com/'
-    return base_url + id
-
-
-def get_preview(id):
-    url = get_url(id)
+def get_previews(id):
+    url = BASE_URL + id
     html = get_html(url)
+    # 没有得到页面html内容就跳出
+    if not html:
+        return None
     soup = BeautifulSoup(html, 'html5lib')
     # 预览图的DOM结构：<a class="sample-box" href=full><div><img src=thumb></div></a>
     samples = soup.find_all(class_='sample-box')
-    preview = []
+    # 没有预览图就跳出
+    if not samples:
+        return None
+    previews = []
     for sample in samples:
         p = {
             'full': sample['href'],
             'thumb': sample.find('img')['src'],
             'title': sample.find('img')['title']
         }
-        preview.append(p)
-    return preview
+        previews.append(p)
+    return previews
 
 
 if __name__ == '__main__':
-    print(get_preview('LKD-004'))
+    print(get_previews('LKD-004'))
+    print(get_previews('MILK-039'))
+    # print(get_html('https://www.javbus.com/LKD-004'))
